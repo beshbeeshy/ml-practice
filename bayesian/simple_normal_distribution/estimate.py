4c4eaa21ef94433217f421e76a6b525a05ab8b76
Bassam Mohammed <basam.mohamed@gmail.com>
2018-04-19 00:07:18 +0800

    Added plotting to bayesian normal distribution

--- bayesian/simple_normal_distribution/estimate.py
+++ bayesian/simple_normal_distribution/estimate.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pymc as pm
 import pprint as pp
+import matplotlib.pyplot as plt
 
 mean_true = 5
-variance_true = 2
+variance_true = 4
 
 data = np.random.normal(mean_true, variance_true, 1000)
 
@@ -15,6 +16,17 @@ likelihood = pm.Normal('likelihood', mu=mean, tau=precision, observed=True, valu
 model = pm.Model([mean, precision, likelihood])
 
 M = pm.MCMC(model)
-M.sample(iter = 40000, burn=5000)
+M.sample(iter = 50000, burn=5000)
 
-pp.pprint(M.stats())
+mean_samples = M.trace('mean')[:]
+precision_samples = M.trace('precision')[:]
+
+ax = plt.subplot(211)
+plt.hist(mean_samples, histtype='stepfilled', bins=50, alpha=0.85,
+         label="mean samples", color="#A60628", normed=True)
+
+ax = plt.subplot(212)
+plt.hist(precision_samples, histtype='stepfilled', bins=50, alpha=0.85,
+         label="precision samples", color="#7A68A6", normed=True)
+
+plt.show()

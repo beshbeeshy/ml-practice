1cbc1a8e75b21e8b12e23f7ce9705aa5bb643ad5
Bassam Mohammed <bassam@Bassams-MacBook-Pro.local>
2018-05-28 21:26:37 +0200

    code cleaning

--- iris-pytorch/iris_pytorch.py
+++ iris-pytorch/iris_pytorch.py
@@ -1,8 +1,8 @@
+import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -23,33 +23,14 @@ class Net(nn.Module):
         xtrained = x
         return x
 
-# training data
-datatrain = pd.read_csv('iris_train.csv')
-datatrain = datatrain[['sepal_length', 'sepal_width', 'petal_width', 'species']]
-datatrain.loc[datatrain['species']=='Iris-setosa', 'species']=0
-datatrain.loc[datatrain['species']=='Iris-versicolor', 'species']=1
-datatrain.loc[datatrain['species']=='Iris-virginica', 'species']=2
-datatrain = datatrain.apply(pd.to_numeric)
-datatrain_array = datatrain.as_matrix()
-xtrain = datatrain_array[:,:3]
-ytrain = datatrain_array[:,3]
-# test data
-datatest = pd.read_csv('iris_test.csv')
-datatest = datatest[['sepal_length', 'sepal_width', 'petal_width', 'species']]
-datatest.loc[datatest['species']=='Iris-setosa', 'species']=0
-datatest.loc[datatest['species']=='Iris-versicolor', 'species']=1
-datatest.loc[datatest['species']=='Iris-virginica', 'species']=2
-datatest = datatest.apply(pd.to_numeric)
-datatest_array = datatest.as_matrix()
-xtest = datatest_array[:,:3]
-ytest = datatest_array[:,3]
-
-xtrained = None
-
-torch.manual_seed(1234)
-
-hl = 3
-num_epoch = 10000
+def load(file_name):
+    data = pd.read_csv(file_name)
+    data = data[['sepal_length', 'sepal_width', 'petal_width', 'species']]
+    data.loc[data['species']=='Iris-setosa', 'species']=0
+    data.loc[data['species']=='Iris-versicolor', 'species']=1
+    data.loc[data['species']=='Iris-virginica', 'species']=2
+    data = data.apply(pd.to_numeric)
+    return data
 
 def train(epoch):
     X = Variable(torch.Tensor(xtrain).float())
@@ -62,8 +43,25 @@ def train(epoch):
     optimizer.step()
 
     if (epoch) % 50 == 0:
-        print ('Epoch [%d/%d] Loss: %.4f' 
-            %(epoch+1, num_epoch, loss.data))
+        print ('Epoch [%d/%d] Loss: %.4f' %(epoch+1, num_epoch, loss.data))
+
+# training data
+datatrain = load('iris_train.csv')
+datatrain_array = datatrain.as_matrix()
+xtrain = datatrain_array[:,:3]
+ytrain = datatrain_array[:,3]
+# test data
+datatest = load('iris_test.csv')
+datatest_array = datatest.as_matrix()
+xtest = datatest_array[:,:3]
+ytest = datatest_array[:,3]
+
+xtrained = None
+
+torch.manual_seed(1234)
+
+hl = 3
+num_epoch = 10000
 
 net = Net()
 
@@ -79,25 +77,14 @@ X = Variable(torch.Tensor(xtest).float())
 Y = torch.Tensor(ytest).long()
 out = net(X)
 _, predicted = torch.max(out.data, 1)
-
+print(out)
 print('Accuracy of the network %d %%' % (100 * torch.sum(Y==predicted) / 30))
 
 fig = plt.figure()
 ax1 = fig.add_subplot(211, projection='3d')
 ax2 = fig.add_subplot(212, projection='3d')
 
-ax1.scatter(
-    datatrain['sepal_length'],
-    datatrain['sepal_width'],
-    datatrain['petal_width'],
-    c=ytrain
-)
-
-ax2.scatter(
-    xtrained_backup[:,0],
-    xtrained_backup[:,1],
-    xtrained_backup[:,2],
-    c=ytrain
-)
+ax1.scatter(datatrain['sepal_length'], datatrain['sepal_width'], datatrain['petal_width'], c=ytrain)
+ax2.scatter(xtrained_backup[:,0], xtrained_backup[:,1], xtrained_backup[:,2], c=ytrain)
 
 plt.show()
